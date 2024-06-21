f5ccbfeb0c74856e828643732d93d508102f6ed5
Chase Meadors <c.ed.mead@gmail.com>
2024-06-20 21:56:33 -0600

    Add plausible

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 // CSS
 import './styles/theme.scss';
 
+import Plausible from 'plausible-tracker';
+Plausible({
+    domain: 'cemulate.github.io/polyomino-solver',
+    apiHost: 'https://plausible.351321.xyz',
+}).enableAutoPageviews();
+
 import 'web-component-polyomino';
 
 import App from './App.svelte';
